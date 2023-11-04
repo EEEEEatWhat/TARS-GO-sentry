c2ba4318dc2e07b37d725b6cb4212e8143b04153
suzukisuncy <chengyangsun79@gmail.com>
2023-11-04 20:01:25 +0800

    add lidar

---
+++ src/sllidar_ros2/launch/sllidar_s1_launch.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    channel_type =  LaunchConfiguration('channel_type', default='serial')
+    serial_port = LaunchConfiguration('serial_port', default='/dev/ttyUSB0')
+    serial_port1 = LaunchConfiguration('serial_port', default='/dev/ttyUSB1')
+    serial_baudrate = LaunchConfiguration('serial_baudrate', default='256000') 
+    frame_id = LaunchConfiguration('frame_id', default='laser')
+    frame_id1 = LaunchConfiguration('frame_id', default='laser1')
+    inverted = LaunchConfiguration('inverted', default='false')
+    angle_compensate = LaunchConfiguration('angle_compensate', default='true')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'channel_type',
+            default_value=channel_type,
+            description='Specifying channel type of lidar'),
+
+        DeclareLaunchArgument(
+            'serial_baudrate',
+            default_value=serial_baudrate,
+            description='Specifying usb port baudrate to connected lidar'),
+
+        DeclareLaunchArgument(
+            'inverted',
+            default_value=inverted,
+            description='Specifying whether or not to invert scan data'),
+
+        DeclareLaunchArgument(
+            'angle_compensate',
+            default_value=angle_compensate,
+            description='Specifying whether or not to enable angle_compensate of scan data'),
+
+        Node(
+            package='sllidar_ros2',
+            executable='sllidar_node',
+            name='sllidar_node',
+            remappings=[("/scan","lidar_rear_left/scan")],
+            parameters=[{'channel_type':channel_type,
+                         'serial_port': serial_port, 
+                         'serial_baudrate': serial_baudrate, 
+                         'frame_id': frame_id,
+                         'inverted': inverted, 
+                         'angle_compensate': angle_compensate}],
+            output='screen'),
+
+        Node(
+            package='sllidar_ros2',
+            executable='sllidar_node',
+            name='sllidar_node1',
+            remappings=[("/scan","lidar_front_right/scan")],
+            parameters=[{'channel_type':channel_type,
+                         'serial_port': serial_port1, 
+                         'serial_baudrate': serial_baudrate, 
+                         'frame_id': frame_id1,
+                         'inverted': inverted, 
+                         'angle_compensate': angle_compensate}],
+            output='screen'),             
+    ])
+
