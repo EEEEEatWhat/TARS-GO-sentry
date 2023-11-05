914b2e54aaea01121b7ac91f68d15990727c5b5e
suzukisuncy <chengyangsun79@gmail.com>
2023-11-05 15:56:00 +0800

    add bringup_launch

---
+++ src/bringup/launch/bringup.launch.py
@@ -0,0 +1,46 @@
+#
+#   created by: suzukisuncy
+#   github.com/suzukisuncy
+#   2023/11/4
+#
+# 导入库
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    """定义LaunchDescription对象"""
+    ld = LaunchDescription()
+
+    """定位功能包"""
+    rplidar_path = get_package_share_directory('sllidar_ros2')
+    merger_path = get_package_share_directory('ros2_laser_scan_merger')
+    matcher_path = get_package_share_directory('ros2_laser_scan_matcher')
+
+    """设置所需路径"""
+    rpliadar_launch_path = os.path.join(rplidar_path, 'launch')
+    merger_launch_path = os.path.join(merger_path, 'launch')
+    matcher_node_path = os.path.join(matcher_path, 'src')
+
+    """添加launch文件"""
+    anotherlaunch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(rpliadar_launch_path, 'sllidar_s1_launch.py')
+        ),
+        PythonLaunchDescriptionSource(
+            os.path.join(merger_launch_path, 'merge_2_scan.launch.py')
+        ),      
+            # launch_arguments={'arg-name': example-arg}.items()
+    )
+    """添加Node节点"""
+    matcher_to_odom = Node(
+        package =  matcher_path,
+        executable = 'laser_scan_matcher' 
+    )
+    """添加启动项目"""
+    ld.add_action(anotherlaunch)
+    ld.add_action(matcher_to_odom)
+    return ld
